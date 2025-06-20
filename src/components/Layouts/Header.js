4956b231208e79619027559fb916909675522796
Bharat2301 <Bharat.tejwani123456@gmail.com>
2025-06-20 18:27:00 +0530

    Solve small bugs

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,36 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import "../../styles/HeaderStyle.css";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo/logo.png";
-//new
-
 
 function Header() {
-  const [nav, setNav] = useState();
-  //Scroll Navbar
+  const [nav, setNav] = useState(false);
 
+  // Scroll Navbar
   const changeValueOnScroll = () => {
-    const scrollValue = document?.documentElement?.scrollTop; scrollValue > 100 ? setNav(true) : setNav(false);
-  }
+    const scrollValue = document?.documentElement?.scrollTop;
+    setNav(scrollValue > 100);
+  };
 
-  window.addEventListener("scroll", changeValueOnScroll);
+  // Attach scroll event listener once
+  useEffect(() => {
+    window.addEventListener("scroll", changeValueOnScroll);
+    return () => window.removeEventListener("scroll", changeValueOnScroll);
+  }, []);
 
   return (
     <header>
-      <Navbar collapseOnSelect expand="lg" className={`${nav === true ? "sticky" : ""}`}>
-
-
+      <Navbar collapseOnSelect expand="lg" className={`${nav ? "sticky" : ""}`}>
         <Container>
-          <Navbar.Brand href="#home">
-            <Link to="/" className="logo">
-              <img src={Logo} alt="Logo" className="imag-fluid" />
-            </Link>
+          <Navbar.Brand as={Link} to="/" className="logo">
+            <img src={Logo} alt="Logo" className="img-fluid" />
           </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/">Home </Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
               <Nav.Link as={Link} to="/about">About</Nav.Link>
               <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
               <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
@@ -38,19 +38,16 @@ function Header() {
               <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
               <Nav.Link as={Link} to="/">
                 <div className="cart">
-                  <i class="bi bi-bag fs-5"> </i>
+                  <i className="bi bi-bag fs-5"></i>
                   <em className="roundpoint">2</em>
                 </div>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
-
         </Container>
       </Navbar>
-
-
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
